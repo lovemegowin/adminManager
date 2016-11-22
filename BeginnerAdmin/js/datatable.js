bb09246f0a726552c696c24bfad94ca6c568cf6c
Van <zheng_jinfan@126.com>
2016-11-22 11:49:38 +0800

    v0.0.3

---
+++ BeginnerAdmin/js/datatable.js
@@ -0,0 +1,25 @@
+layui.define(['layer', 'laypage'], function(exports) {
+	"use strict";
+
+	function($,layer,laypage){
+		var elem = {
+			table:'beg-table',
+			hover : 'beg-table-hover'
+		};
+		var DataTable = function(){
+			
+			this.config={
+				
+			};
+		}
+		
+		
+		
+		
+	}(layui.jquery, layui.layer, layui.laypage);	
+	
+	
+	
+	
+	exports('datatable', null);
+});
