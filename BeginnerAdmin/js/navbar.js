561f6539e6f2e418413494e18c9fd48de152a459
Beginner <zheng_jinfan@126.com>
2016-11-25 17:38:03 +0800

    完善Navbar组件...

--- BeginnerAdmin/js/navbar.js
+++ BeginnerAdmin/js/navbar.js
@@ -12,23 +12,22 @@ layui.config({
 		 *  默认配置 
 		 */
 		this.config = {
-			elem: undefined,
-			data: undefined,
-			href: undefined,
-			type: 'GET'
-		}
+			elem: undefined, //容器
+			data: undefined, //数据源
+			url: undefined, //数据源地址
+			type: 'GET' //读取方式
+		};
+		this.v = '0.0.1';
 	};
 	Navbar.prototype.render = function() {
 		var _that = this;
-
 		var _config = _that.config;
-
 		if(typeof(_config.elem) !== 'string' && typeof(_config.elem) !== 'object') {
 			common.throwError('Navbar error: elem参数未定义或设置出错，具体设置格式请参考文档API.');
 		}
 		var $container;
 		if(typeof(_config.elem) === 'string') {
-			$container = $(_config.elem);
+			$container = $('' + _config.elem + '');
 		}
 		if(typeof(_config.elem) === 'object') {
 			$container = _config.elem;
@@ -36,36 +35,35 @@ layui.config({
 		if($container.length === 0) {
 			common.throwError('Navbar error:找不到elem参数配置的容器，请检查.');
 		}
-		if(_config.data === undefined && _config.href === undefined) {
+		if(_config.data === undefined && _config.url === undefined) {
 			common.throwError('Navbar error:请为Navbar配置数据源.')
 		}
 		if(_config.data !== undefined && typeof(_config.data) === 'object') {
 			var html = getHtml(_config.data);
 			$container.html(html);
 			element.init();
-		}
-		//渲染nav
-		/*$.getJSON('datas/nav.json', null, function(data) {
-			var li = '';
-			for(var i = 0; i < data.length; i++) {
-				li += '<li class="layui-nav-item"><a href="javascript:;">' + data[i].title + '</a>'
-				if(data[i].children !== undefined && data[i].children.length > 0) {
-					li += '<dl class="layui-nav-child">'
-					for(var j = 0; j < data[i].child.length; j++) {
-						li += '<dd>' +
-							'<a href="javascript:;" data-url="' + data[i].children[j].href + '">' +
-							'<i class="fa ' + data[i].children[j].icon + '" aria-hidden="true"></i>' +
-							'<cite>' + data[i].children[j].title + '</cite>' +
-							'</a>' +
-							'</dd>';
-					}
-					li += '</dl>';
+			_that.config.elem = $container;
+		} else {
+			$.ajax({
+				type: _config.type,
+				url: _config.url,
+				async: false,//_config.async,
+				dataType: 'json',
+				success: function(result, status, xhr) {
+					var html = getHtml(result);
+					$container.html(html);
+					element.init();
+					_that.config.isLoadSuccess = true;
+				},
+				error: function(xhr, status, error) {
+					common.msgError('Navbar error:' + error);
+				},
+				complete: function(xhr, status) {
+					_that.config.elem = $container;
 				}
-				li += '</li>';
-			}
-			$('#dynamic').html(li);
-			element.init();
-		});*/
+			});
+		}
+		return _that;
 	};
 	/**
 	 * 配置Navbar
@@ -76,9 +74,73 @@ layui.config({
 		$.extend(true, that.config, options);
 		return that;
 	};
+	Navbar.prototype.on = function(events, callback) {
+		var that = this;
+		var _con = that.config.elem;
+		/*setTimeout(function() {
+			console.log(that.config.isLoadSuccess)
+		}, 2000);*/
+		if(typeof(events) !== 'string') {
+			common.throwError('Navbar error:事件名配置出错，请参考API文档.');
+		}
+		var lIndex = events.indexOf('(');
+		var eventName = events.substr(0, lIndex);
+		var filter = events.substring(lIndex + 1, events.indexOf(')'));
+		if(eventName === 'click') {
+			if(_con.attr('lay-filter') !== undefined) {
+				_con.children('ul').find('li').each(function() {
+					var $this = $(this);
+					if($this.find('dl').length > 0) {
+						var $dd = $this.find('dd').each(function() {
+							$(this).on('click', function() {
+								var $a = $(this).children('a');
+								var href = $a.data('url');
+								var icon = '';
+								var title = $a.children('cite').text();
+								var data = {
+									elem: $a,
+									field: {
+										href: href,
+										icon: icon,
+										title: title
+									}
+								}
+								callback(data);
+							});
+						});
+					} else {
+						$this.on('click', function() {
+							var $a = $this.children('a');
+							var href = $a.data('url');
+							var icon = '';
+							var title = $a.children('cite').text();
+							var data = {
+								elem: $a,
+								field: {
+									href: href,
+									icon: icon,
+									title: title
+								}
+							}
+							callback(data);
+						});
+					}
+				});
+			}
+		}
+		//callback('aa');
 
+		//return layui.onevent('navbar',events,callback);
+	};
+
+	function bindEvent($navbar, events, callback) {
+
+	}
+	/**
+	 * 获取html字符串
+	 * @param {Object} data
+	 */
 	function getHtml(data) {
-		console.log(data.length);
 		var ulHtml = '<ul class="layui-nav layui-nav-tree">';
 		for(var i = 0; i < data.length; i++) {
 			ulHtml += '<li class="layui-nav-item">';
