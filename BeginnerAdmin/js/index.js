9bcc510394660932faa5e081ac32e018ce43716c
Beginner <zheng_jinfan@126.com>
2016-11-27 12:02:28 +0800

    beta v0.0.6

---
+++ BeginnerAdmin/js/index.js
@@ -0,0 +1,53 @@
+layui.config({
+	base: 'js/'
+}).use(['element', 'layer', 'navbar', 'tab'], function() {
+	var element = layui.element(),
+		$ = layui.jquery,
+		layer = layui.layer,
+		navbar = layui.navbar(),
+		tab = layui.tab({
+			elem: '.admin-nav-card' //设置选项卡容器
+		});
+
+	//iframe自适应
+	$(window).on('resize', function() {
+		var $content = $('.admin-nav-card .layui-tab-content');
+		$content.height($(this).height() - 147);
+		$content.find('iframe').each(function() {
+			$(this).height($content.height());
+		});
+	}).resize();
+	//设置navbar
+	navbar.set({
+		elem: '#admin-navbar-side',
+		url: 'datas/nav.json'
+	});
+	//渲染navbar
+	navbar.render();
+	//监听点击事件
+	navbar.on('click(side)', function(data) {
+		tab.tabAdd(data.field);
+	});
+
+	$('.admin-side-toggle').on('click', function() {
+		var sideWidth = $('#admin-side').width();
+		if(sideWidth === 200) {
+			$('#admin-side').width(0);
+			$('#admin-body').css('left', '0');
+		} else {
+			$('#admin-side').width(200);
+			$('#admin-body').css('left', '200px');
+		}
+
+	});
+
+	//手机设备的简单适配
+	var treeMobile = $('.site-tree-mobile'),
+		shadeMobile = $('.site-mobile-shade');
+	treeMobile.on('click', function() {
+		$('body').addClass('site-mobile');
+	});
+	shadeMobile.on('click', function() {
+		$('body').removeClass('site-mobile');
+	});
+});
--- BeginnerAdmin/js/navbar.js
+++ BeginnerAdmin/js/navbar.js
@@ -1,6 +1,6 @@
 layui.config({
 	base: 'js/'
-}).define(['element', 'layer', 'common'], function(exports) {
+}).define(['element', 'common'], function(exports) {
 	"use strict";
 	var $ = layui.jquery,
 		layer = parent.layer === undefined ? layui.layer : parent.layer,
@@ -91,7 +91,7 @@ layui.config({
 							$(this).on('click', function() {
 								var $a = $(this).children('a');
 								var href = $a.data('url');
-								var icon = '';
+								var icon = $a.children('i:first').data('icon');
 								var title = $a.children('cite').text();
 								var data = {
 									elem: $a,
@@ -109,7 +109,7 @@ layui.config({
 						$this.on('click', function() {
 							var $a = $this.children('a');
 							var href = $a.data('url');
-							var icon = '';
+							var icon = $a.children('i:first').data('icon');
 							var title = $a.children('cite').text();
 							var data = {
 								elem: $a,
@@ -156,9 +156,9 @@ layui.config({
 					ulHtml += '<a href="javascript:;" data-url="' + data[i].children[j].href + '">';
 					if(data[i].children[j].icon !== undefined && data[i].children[j].icon !== '') {
 						if(data[i].children[j].icon.indexOf('fa-') !== -1) {
-							ulHtml += '<i class="fa ' + data[i].children[j].icon + '" aria-hidden="true"></i>';
+							ulHtml += '<i class="fa ' + data[i].children[j].icon + '" data-icon="' + data[i].children[j].icon + '" aria-hidden="true"></i>';
 						} else {
-							ulHtml += '<i class="layui-icon">' + data[i].children[j].icon + '</i>';
+							ulHtml += '<i class="layui-icon" data-icon="' + data[i].children[j].icon + '">' + data[i].children[j].icon + '</i>';
 						}
 					}
 					ulHtml += '<cite>' + data[i].children[j].title + '</cite>';
